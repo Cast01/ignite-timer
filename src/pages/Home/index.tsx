81f663acee3172ab721639ae2514f310628ae244
Cast01 <gabriel_floriano_castilho@outlook.com>
2023-06-06 20:27:43 -0300

    titulo da página reativo

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -56,6 +56,7 @@ export function Home() {
 
     setCycles((oldCycles) => [...oldCycles, newCycle]);
     setActiveCycleId(id);
+    setAmountSecondsPassed(0);
 
     reset();
   }
@@ -63,13 +64,19 @@ export function Home() {
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
 
   useEffect(() => {
+    let interval: number;
+
     if (activeCycle) {
-      setInterval(() => {
+      interval = setInterval(() => {
         setAmountSecondsPassed(
           differenceInSeconds(new Date(), activeCycle.startDate),
         );
       }, 1000);
     }
+
+    return () => {
+      clearInterval(interval);
+    };
   }, [activeCycle]);
 
   const totalSeconds = activeCycle ? activeCycle.minutesAmmount * 60 : 0;
@@ -80,6 +87,12 @@ export function Home() {
 
   const minutes = String(minutesAmount).padStart(2, '0');
   const seconds = String(secondsAmount).padStart(2, '0');
+
+  useEffect(() => {
+    if (activeCycle) {
+      document.title = `${minutes} : ${seconds}`;
+    }
+  }, [minutes, seconds, activeCycle]);
 
   console.log({
     activeCycle,
